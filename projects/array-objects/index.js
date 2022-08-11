33cbf76e4df2c40ef93c78130748a1688e54a6f2
Elena <golyasovaeo@gmail.com>
2022-08-11 22:29:57 +0500

    написала функцию reduce

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,12 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +25,13 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = fn(array[i], i, array);
+  }
+  return newArray;
+}
 
 /*
  Задание 3:
@@ -31,7 +42,40 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let previousValue;
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0 && initial !== undefined) {
+      previousValue = fn(initial, array[i], i, array);
+    } else if (i === 0 && initial === undefined) {
+      previousValue = array[i];
+    } else {
+      previousValue = fn(previousValue, array[i], i, array);
+    }
+    fn(previousValue, array[i], i, array);
+  }
+  console.log(previousValue);
+  return previousValue;
+}
+// for (let i=0; i < array.length; i++){
+//   let previousValue = 0;
+//   if(i === 0) {
+//     previousValue = fn(initial, array[i], i, array)
+//   } else {
+//     previousValue = fn( previousValue ? previousValue : array[i], array[i], i, array );
+//     fn( previousValue, array[i], i, array)
+//   }
+// }
+// return fn();
+// }
+reduce([1, 2, 3], (all, current) => all + current);
+
+// let summ = 0;
+// for (let i=0; i < array.length; i++){
+//   summ += array[i];
+// }
+// return summ + initial;
+// reduce([1, 2, 3], (all, current) => all + current);
 
 /*
  Задание 4:
@@ -41,7 +85,14 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const array = [];
+  for (let prop of Object.keys(obj)) {
+    prop = prop.toUpperCase();
+    array.push(prop);
+  }
+  return array;
+}
 
 /*
  Задание 5 *:
@@ -54,6 +105,14 @@ function upperProps(obj) {}
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  obj = new Proxy(obj, {
+    set(target, prop, val) {
+      target[prop] = val * val;
+      return true;
+    },
+  });
+  return obj;
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
